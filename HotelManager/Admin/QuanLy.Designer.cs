ba798ff7e933794160a97da50d9175eb63ab238e
KienTran <trandotrungkien@gmail.com>
2023-03-24 01:48:24 +0700

    login and db

--- HotelManager/Admin/QuanLy.Designer.cs
+++ HotelManager/Admin/QuanLy.Designer.cs
@@ -28,33 +28,54 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
+            this.panelFunction = new System.Windows.Forms.Panel();
+            this.panelInfo = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panelFunction.SuspendLayout();
             this.SuspendLayout();
             // 
-            // label1
+            // panelFunction
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(336, 106);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(36, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Admin";
+            this.panelFunction.Controls.Add(this.button1);
+            this.panelFunction.Location = new System.Drawing.Point(-1, -1);
+            this.panelFunction.Name = "panelFunction";
+            this.panelFunction.Size = new System.Drawing.Size(216, 510);
+            this.panelFunction.TabIndex = 0;
+            // 
+            // panelInfo
+            // 
+            this.panelInfo.Location = new System.Drawing.Point(-1, 507);
+            this.panelInfo.Name = "panelInfo";
+            this.panelInfo.Size = new System.Drawing.Size(216, 85);
+            this.panelInfo.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(0, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(216, 83);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // QuanLy
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.label1);
+            this.ClientSize = new System.Drawing.Size(988, 591);
+            this.Controls.Add(this.panelInfo);
+            this.Controls.Add(this.panelFunction);
             this.Name = "QuanLy";
             this.Text = "QuanLy";
+            this.panelFunction.ResumeLayout(false);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panelFunction;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panelInfo;
     }
 }
